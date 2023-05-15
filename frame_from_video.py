54c158c6336e28f142691fb0bf7402ef328ca9c6
Prasheel Vellanki <prasheelvellanki@gmail.com>
2023-05-15 12:49:41 -0400

    Formatting and flexible naming

--- frame_from_video.py
+++ frame_from_video.py
@@ -1,37 +1,36 @@
 import cv2
 
+spec_name = "red_no_holes_covered_2"
 
-video = cv2.VideoCapture('./videos/video_red_no_holes_covered_2.mov')
+video = cv2.VideoCapture(f"./videos/video_{spec_name}.mov")
 
-# Initialize a counter variable
+
 frame_count = 0
 
-# Loop through the video frames
+
 while True:
-    # Read the next frame from the video
+    
     ret, frame = video.read()
 
     isOpen = video.isOpened()
     print(isOpen)
-    # If there are no more frames, break out of the loop
+   
     if not ret:
-        
         break
 
-    # Increment the counter variable
     frame_count += 1
 
-    # Save the current frame as an image file
-    filename = f'./frame_caps/red_no_holes_{frame_count}.jpg'
+
+    filename = f'./frame_caps/{spec_name}_{frame_count}.jpg'
     cv2.imwrite(filename, frame)
 
-    # Display the frame
+
     cv2.imshow('Frame', frame)
 
-    # Wait for a key press and check if the user wants to quit
+    # wait for a key press and check if the user wants to quit
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the video and close all windows
+
 video.release()
 cv2.destroyAllWindows()
